398af8777e4fd8c50a1d8282a8f7ceff383821d6
GVAmaresh <reshama0302@gmail.com>
2024-10-10 00:55:37 +0530

    created the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
           <head>
-          <link rel="manifest" href="manifest.json" />
+          <link rel="manifest" href="/app/manifest.json" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
